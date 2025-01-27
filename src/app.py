66c33a08a4b181dfd4937f432ed1bc44dace9915
derichard <16598223+derichard@users.noreply.github.com>
2025-01-27 14:36:12 +0100

    adapt size

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from window import Window, Point, Line, Cell, Maze
 
 def main():
     win = Window(1024, 900)
-    maze = Maze(10, 10, 20, 20, 40, 40, win, seed=0, solve_algo="bfs")
+    maze = Maze(10, 10, 25, 25, 40, 40, win, seed=None, solve_algo="dfs", solve_speed=0.1)
     maze.solve()
     win.wait_for_close()
 
